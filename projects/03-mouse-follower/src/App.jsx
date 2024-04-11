7a3fb82a5b4f52f9251e23d17471bee1604cd12b
RamiroVega <ramiro.vega1993@gmail.com>
2024-04-10 23:57:04 -0500

    Se realizo efecto de seguir cursor

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,13 +1,49 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({x:0, y:0})
+
+  useEffect (() =>{
+    console.log('Efecto', {enabled});
+
+    const handleMove = (event) =>{
+      const {clientX, clientY} = event
+      console.log('handleMove', {clientX, clientY});
+      setPosition({x: clientX, y: clientY})
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+    return () =>{
+      console.log('limpiar');
+      window.removeEventListener('pointermove', handleMove)
+    }   
+  }, [enabled])
 
   return (
-    <h3>
-      Proyecto 3
-    </h3>
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px,${position.y}px )`
+      }}/>
+      <button onClick={()=> setEnabled(!enabled)}> {
+        enabled ? 'Desactivar' : 'Activar'} Seguir Puntero
+      </button>
+    </main>
+    
   )
 }
 
